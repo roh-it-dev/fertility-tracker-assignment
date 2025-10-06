74ea8de0bc796dc8ee722df914b11130ccb71676
rohit <rohit.limba@inito.com>
2025-10-06 12:26:43 +0530

    changes for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,21 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
-require('dotenv').config();
+const knex = require('knex');
 
-module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: process.env.DB_PORT
-    },
-    migrations: { directory: './migrations' }
+const db = knex({
+  client: 'pg', 
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT)
+  },
+  migrations: {
+    directory: './migrations'
   }
-};
+});
+
+module.exports = db;
